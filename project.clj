7c15526dc76340013a3fabc93876fb98560c4220
Jeff Weiss <jweiss@redhat.com>
2011-12-01 10:49:49 -0500

    revert back to 1.2.1 - won't affect downstream projects

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.weissjeffm/serializable-fn "1.2"
+(defproject org.clojars.weissjeffm/serializable-fn "1.2.1-SNAPSHOT"
   :url "http://github.com/technomancy/serializable-fn"
   :author "Seajure"
   :description "Make functions that have pretty representations."
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
